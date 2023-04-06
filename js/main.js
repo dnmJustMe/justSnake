00100fde1bd46748cfbcc9e11dfbe9a91857fae2
DNMjustMe <dan.nm9906@gmail.com>
2023-04-06 10:36:05 -0400

    changing head animation

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@ const startButton = document.getElementById("startButton");
 const startWelcome = document.getElementById("startWelcome");
 const gameOverSign = document.getElementById("gameOver");
 const scoreOpinion = document.getElementById("scoreOpinion");
+const scoreSign = document.getElementById("score");
 const highScore = document.getElementById("highScore");
 const highScoreBoard = document.getElementById("highScoreBoard");
 const playAgainButton = document.getElementById("playAgainButton");
 const controlsLeft = document.getElementById("controlsLeft");
 const controlsRight = document.getElementById("controlsRight");
 
-
 const boardSize = 10;
-const gameSpeed = 200;
+var gameSpeed = 200;
 const squareTypes = {
   emptySquare: 0,
   snakeSquare: 1,
@@ -36,41 +36,119 @@ let emptySquares;
 let moveInterval;
 
 const drawSnake = () => {
+  const cabeza = String(
+    Number(snake[snake.length - 1])).padStart(2, "0");
+  const [rowCabeza, columnCabeza] = cabeza.split("");
+
+  const delanteCabeza = String(
+    Number(snake[snake.length - 1]) + directions[direction]
+  ).padStart(2, "0");
+  const [rowDelanteCabeza, columnDelanteCabeza] = delanteCabeza.split("");
+
+  var head
+ 
+
+  if(delanteCabeza > 0){
+    if (boardSquares[rowDelanteCabeza][columnDelanteCabeza] === squareTypes.foodSquare && delanteCabeza > 0 || boardSquares[rowCabeza][columnCabeza] === squareTypes.foodSquare) {
+      head = "snakeSquareHeadOpen";
+    } else {
+      head = "snakeSquareHead";
+    }
+  }else{
+    if (boardSquares[rowCabeza][columnCabeza] === squareTypes.foodSquare) {
+      head = "snakeSquareHeadOpen";
+    } else {
+      head = "snakeSquareHead";
+    }
+  }
+
   snake.forEach(function (square) {
     drawSquare(square, "snakeSquare");
-    var ultimo1 = snake[0][0];
-    var ultimo2 = snake[0][1];
-    var penultimo1 = snake[1][0];
-    var penultimo2 = snake[1][1];
+    var ultimo0 = snake[0][0];
+    var ultimo1 = snake[0][1];
+    var penultimo0 = snake[1][0];
+    var penultimo1 = snake[1][1];
 
     switch (direction) {
       case "ArrowRight":
-        drawSquare(
-          snake[snake.length - 1],
-          "snakeSquareHead movementHeadRight"
-        );
+        drawSquare(snake[snake.length - 1], head + " movementHeadRight");
         break;
       case "ArrowLeft":
-        drawSquare(snake[snake.length - 1], "snakeSquareHead movementHeadLeft");
+        drawSquare(snake[snake.length - 1], head + " movementHeadLeft");
         break;
       case "ArrowDown":
-        drawSquare(snake[snake.length - 1], "snakeSquareHead movementHeadDown");
+        drawSquare(snake[snake.length - 1], head + " movementHeadDown");
         break;
       case "ArrowUp":
-        drawSquare(snake[snake.length - 1], "snakeSquareHead movementHeadUp");
+        drawSquare(snake[snake.length - 1], head + " movementHeadUp");
         break;
     }
 
-    if (ultimo1 == penultimo1 && ultimo2 < penultimo2) {
+    if (ultimo0 == penultimo0 && ultimo1 < penultimo1) {
       drawSquare(snake[0], "snakeSquareTail movementTailRight");
-    } else if (ultimo1 == penultimo1 && ultimo2 > penultimo2) {
+    } else if (ultimo0 == penultimo0 && ultimo1 > penultimo1) {
       drawSquare(snake[0], "snakeSquareTail movementTailLeft");
-    } else if (ultimo2 == penultimo2 && ultimo1 > penultimo1) {
+    } else if (ultimo1 == penultimo1 && ultimo0 > penultimo0) {
       drawSquare(snake[0], "snakeSquareTail movementTailUp");
-    } else if (ultimo2 == penultimo2 && ultimo1 < penultimo1) {
+    } else if (ultimo1 == penultimo1 && ultimo0 < penultimo0) {
       drawSquare(snake[0], "snakeSquareTail movementTailDown");
     }
   });
+  for (let index = 1; index < snake.length - 1; index++) {
+    const element = snake[index];
+    var anterior = snake[index - 1];
+    var proximo = snake[index + 1];
+
+    if (
+      anterior[0] == element[0] &&
+      anterior[1] < element[1] &&
+      element[0] > proximo[0]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareRightUp");
+    } else if (
+      anterior[0] == element[0] &&
+      anterior[1] < element[1] &&
+      element[0] < proximo[0]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareRightDown");
+    } else if (
+      anterior[0] == element[0] &&
+      anterior[1] > element[1] &&
+      element[0] > proximo[0]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareLeftUp");
+    } else if (
+      anterior[0] == element[0] &&
+      anterior[1] > element[1] &&
+      element[0] < proximo[0]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareLeftDown");
+    } else if (
+      anterior[1] == element[1] &&
+      anterior[0] > element[0] &&
+      element[1] < proximo[1]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareUpRight");
+    } else if (
+      anterior[1] == element[1] &&
+      anterior[0] > element[0] &&
+      element[1] > proximo[1]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareUpLeft");
+    } else if (
+      anterior[1] == element[1] &&
+      anterior[0] < element[0] &&
+      element[1] < proximo[1]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareDownRight");
+    } else if (
+      anterior[1] == element[1] &&
+      anterior[0] < element[0] &&
+      element[1] > proximo[1]
+    ) {
+      drawSquare(snake[index], "snakeSquare movementSquareDownLeft");
+    }
+  }
 };
 
 const drawSquare = (square, type) => {
@@ -121,28 +199,29 @@ const addFood = () => {
 };
 
 const gameOver = () => {
-  var touchDevice = ('ontouchstart' in document.documentElement);
+  var touchDevice = "ontouchstart" in document.documentElement;
   if (touchDevice) {
-    controlsLeft.style.display = "none"
-    controlsRight.style.display = "none"
-    page.style.flexDirection = "column"
+    controlsLeft.style.display = "none";
+    controlsRight.style.display = "none";
+    page.style.flexDirection = "column";
   }
 
-    if (localStorage.highScore) {
-      if (localStorage.highScore < score) {
-        localStorage.highScore = score;
-        scoreOpinion.innerText = "Felicidades, has logrado una nueva mejor puntuación!!!"
-      }else{
-        scoreOpinion.innerText = "Puedes hacerlo mejor..."
-      }
-    } else {
+  if (localStorage.highScore) {
+    if (localStorage.highScore < score) {
       localStorage.highScore = score;
+      scoreOpinion.innerText =
+        "Felicidades, has logrado una nueva mejor puntuación!!!";
+    } else {
+      scoreOpinion.innerText = "Puedes hacerlo mejor...";
     }
+  } else {
+    localStorage.highScore = score;
+  }
 
   game.style.display = "none";
   gameOverSign.style.display = "flex";
+  scoreSign.innerText = score;
   clearInterval(moveInterval);
-
 
   highScore.innerText = localStorage.highScore;
 };
@@ -168,18 +247,18 @@ const directionEvent = (key) => {
   }
 };
 
-$('#down').click(function () {
-  setDirection('ArrowDown')
-})
-$('#up').click(function () {
-  setDirection('ArrowUp')
-})
-$('#right').click(function () {
-  setDirection('ArrowRight')
-})
-$('#left').click(function () {
-  setDirection('ArrowLeft')
-})
+$("#down").click(function () {
+  setDirection("ArrowDown");
+});
+$("#up").click(function () {
+  setDirection("ArrowUp");
+});
+$("#right").click(function () {
+  setDirection("ArrowRight");
+});
+$("#left").click(function () {
+  setDirection("ArrowLeft");
+});
 
 const createRandomFood = () => {
   const randomEmptySquare =
@@ -189,6 +268,9 @@ const createRandomFood = () => {
 
 const updateScore = () => {
   scoreBoard.innerText = score;
+  if (score != 0 && score < 20) {
+    gameSpeed = gameSpeed - 10;
+  }
 };
 
 const createBoard = () => {
@@ -205,6 +287,7 @@ const createBoard = () => {
 };
 
 const setGame = () => {
+  gameSpeed = 200;
   snake = ["00", "01", "02", "03"];
   score = snake.length - 4;
   direction = "ArrowRight";
@@ -217,12 +300,12 @@ const setGame = () => {
 };
 
 const startGame = () => {
-  var touchDevice = ('ontouchstart' in document.documentElement);
-if (touchDevice) {
-  controlsLeft.style.display = "block"
-  controlsRight.style.display = "block"
-  page.style.flexDirection = "row"
-}
+  var touchDevice = "ontouchstart" in document.documentElement;
+  if (touchDevice) {
+    controlsLeft.style.display = "block";
+    controlsRight.style.display = "block";
+    page.style.flexDirection = "row";
+  }
   let game = document.getElementById("game");
   game.style.display = "block";
   setGame();
